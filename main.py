e68585e8e59c6017fe683f411af43ba443967297
Arvind <a.shyam.2313@gmail.com>
2023-03-24 11:13:45 -0400

    added comment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from midiutil import MIDIFile
 from mingus.core import chords
 from mingus.core.mt_exceptions import FormatError
 import random
-
+# Install libraries 
 
 def convert(note, octave):
     # converts a note to a MIDI number
@@ -20,9 +20,6 @@ with open("transition_matrix.txt", "r") as tm:
 
     # generated a 2D list - each nth sublist contains the chords from the nth song.
     a = tm.read()
-
-
-    
     a = a.split(")")
     for x in range(len(a)):
         a[x] = a[x].replace("'", "")
