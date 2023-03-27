f544b983386734fe7b6c73e49370c17a03ab7413
Arvind <a.shyam.2313@gmail.com>
2023-03-27 18:19:22 -0400

    fixed chord class

--- Chord.py
+++ Chord.py
@@ -10,7 +10,7 @@ class Chord:
         self.NOTES = {"B#": 0, "C": 0, "C#": 1, "Db": 1, "D": 2, "D#": 3, "Eb": 3,
          "E": 4, "Fb": 4, "E#": 5, "F": 5, "F#": 6, "Gb": 6, "G": 7,
          "G#": 8, "Ab": 8, "A": 9, "A#": 10, "Bb": 10, "B": 11, "Cb": 11}
-        self.midi = self.convert()
+        self.midi = [self.convert(note) for note in self.notes]
 
         
     def generate_notes(self):
@@ -28,17 +28,15 @@ class Chord:
             self.notes.pop(0)
             self.notes.append(to_remove)
 
-    def convert(self):
-        for note in self.notes:
-            # converts a note to a MIDI number
-            if "bb" in note[0]:
-                note[0] = note[0].replace("bb", "")
-                return (self.NOTES[note[0]] - 2) + 12 * (self.octave + 1)
-            if "##" in note[0]:
-                note[0] = note[0].replace("##", "")
-                return (self.NOTES[note[0]] + 2) + 12 * (self.octave + 1)
-            return self.NOTES[note[0]] + 12 * (self.octave + 1)
+    def convert(self, note):
+        # converts a note to a MIDI number
+        if "bb" in note[0]:
+            note[0] = note[0].replace("bb", "")
+            return (self.NOTES[note[0]] - 2) + 12 * (self.octave + 1)
+        if "##" in note[0]:
+            note[0] = note[0].replace("##", "")
+            return (self.NOTES[note[0]] + 2) + 12 * (self.octave + 1)
+        return self.NOTES[note[0]] + 12 * (self.octave + 1)
 
 a = Chord("Cmaj7", inversion=1)
-a.apply_inversion()
-print(a.notes)
+print(a.midi)
