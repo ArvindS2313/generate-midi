b60a939cd4d3f688500a6cde5d42b373b73009f1
Arvind <a.shyam.2313@gmail.com>
2023-03-29 18:47:44 -0400

    changed file

--- Chord.py
+++ Chord.py
@@ -7,8 +7,8 @@ class Chord:
         self.octave = octave
         self.inversion = inversion
         self.notes = self.generate_notes()
-        self.NOTES = {"B#": 0, "C": 0, "C#": 1, "Db": 1, "D": 2, "D#": 3, "Eb": 3,
-         "E": 4, "Fb": 4, "E#": 5, "F": 5, "F#": 6, "Gb": 6, "G": 7,
+        self.NOTES = {"B#": 0, "C": 0, "C#": 1, "Db": 1, "D": 2, "D#": 3,
+         "Eb": 3, "E": 4, "Fb": 4, "E#": 5, "F": 5, "F#": 6, "Gb": 6, "G": 7,
          "G#": 8, "Ab": 8, "A": 9, "A#": 10, "Bb": 10, "B": 11, "Cb": 11}
         self.midi = [self.convert(note) for note in self.notes]
 
