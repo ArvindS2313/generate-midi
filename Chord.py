e430776b63e237ac2b0cde3b217c8ec71fe7e7b5
Arvind <a.shyam.2313@gmail.com>
2023-04-02 18:36:41 -0400

    modified chord.py

--- Chord.py
+++ Chord.py
@@ -2,12 +2,13 @@ from mingus.core import chords
 from mingus.core.mt_exceptions import FormatError
 
 class Chord:
-    def __init__(self, name, octave=4) -> None:
+    def __init__(self, name, octave=4, inversion=0) -> None:
         self.name = name
         self.octave = octave
+        self.inversion = inversion
         self.notes = self.generate_notes()
-        self.NOTES = {"B#": 0, "C": 0, "C#": 1, "Db": 1, "D": 2, "D#": 3,
-         "Eb": 3, "E": 4, "Fb": 4, "E#": 5, "F": 5, "F#": 6, "Gb": 6, "G": 7,
+        self.NOTES = {"B#": 0, "C": 0, "C#": 1, "Db": 1, "D": 2, "D#": 3, "Eb": 3,
+         "E": 4, "Fb": 4, "E#": 5, "F": 5, "F#": 6, "Gb": 6, "G": 7,
          "G#": 8, "Ab": 8, "A": 9, "A#": 10, "Bb": 10, "B": 11, "Cb": 11}
         self.midi = [self.convert(note) for note in self.notes]
 
@@ -20,6 +21,13 @@ class Chord:
         notes = [[note,self.octave] for note in notes]
         return notes
 
+    def apply_inversion(self):
+        for i in range(self.inversion):
+            to_remove = self.notes[0]
+            to_remove[1] += 1 
+            self.notes.pop(0)
+            self.notes.append(to_remove)
+
     def convert(self, note):
         # converts a note to a MIDI number
         if "bb" in note[0]:
@@ -29,4 +37,3 @@ class Chord:
             note[0] = note[0].replace("##", "")
             return (self.NOTES[note[0]] + 2) + 12 * (self.octave + 1)
         return self.NOTES[note[0]] + 12 * (self.octave + 1)
-
