4253e1b43764c2083b7ef210a70d1c5ac7171c45
Arvind <a.shyam.2313@gmail.com>
2023-04-12 19:08:23 -0400

    worked on section file

--- Section.py
+++ Section.py
@@ -23,29 +23,6 @@ class Section():
                 if random_number <= curr_sum:
                     return Chord(key)
 
-    def generate_pattern(self, segment, starter=None, num_chords=4):
-        pass
-        # first_chord = self.get_chord(starter)
-        # first_chord_name = first_chord.name
-        # chord_sequence = [first_chord]
-        # # generate the entire chord sequence
-        # sum = 0
-        # for i in range(num_chords-1):
-        #     for key in chord_tm[first_chord_name].keys():
-        #         sum += chord_tm[first_chord_name][key]
-        #     random_number = random.randint(1, sum)
-        #     curr_sum = 0
-        #     for key in chord_tm[first_chord_name].keys():
-        #         curr_sum += chord_tm[first_chord_name][key]
-        #         if random_number <= curr_sum:
-        #             sum = 0
-        #             chord_sequence.append(Chord(key))
-        #             first_chord = key
-        #             break
-        # for i in range(len(self.pattern)):
-        #     if self.pattern[i] == segment:
-        #         self.segments[i] = chord_sequence
-
     def set_pattern(self, segment, chord_sequence):
         for i in range(len(self.pattern)):
             if self.pattern[i] == segment:
